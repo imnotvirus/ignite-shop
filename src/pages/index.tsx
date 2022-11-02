efccd5ef74b3df0f5c762dfd9afb76c2240a4a77
Luiz Claudio <lcluizclaudio96@gmail.com>
2022-11-02 19:23:03 -0300

    stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import { styled } from "../styles";
+
+const Button = styled("button", {
+  backgroundColor: "$rocketseat",
+});
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
