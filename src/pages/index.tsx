8447727ebae671dd472b905fe00b4fcba9075e73
Luiz Claudio <lcluizclaudio96@gmail.com>
2022-11-02 20:29:14 -0300

    carousel with keen slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,44 @@
+import { useKeenSlider } from "keen-slider/react";
 import Image from "next/image";
-import { HomeContainer, Product } from "../styles/pages/home";
-
 import camiseta1 from "../assets/1.png";
 import camiseta2 from "../assets/2.png";
 import camiseta3 from "../assets/3.png";
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import "keen-slider/keen-slider.min.css";
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 2.3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="camiseta1" />
         <footer>
           <strong>Camiseta X</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta2} width={520} height={480} alt="camiseta2" />
         <footer>
           <strong>Camiseta X</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="camiseta3" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta3} width={520} height={480} alt="camiseta3" />
         <footer>
           <strong>Camiseta X</strong>
