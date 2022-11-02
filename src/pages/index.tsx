bb054ade2481960a09a9e63c2f603e4cb1d5ee3d
Luiz Claudio <lcluizclaudio96@gmail.com>
2022-11-02 19:54:41 -0300

    style product

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,34 @@
-import { styled } from "../styles";
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
 
-const Button = styled("button", {
-  backgroundColor: "$rocketseat",
-});
+import camiseta1 from "../assets/1.png";
+import camiseta2 from "../assets/2.png";
+import camiseta3 from "../assets/3.png";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <Button>Click me</Button>
-    </div>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="camiseta1" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="camiseta2" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta3} width={520} height={480} alt="camiseta3" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
